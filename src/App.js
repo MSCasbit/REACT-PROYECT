118231b8995df8be5b145e91d374f59250ba85ca
mauricio <castillo_smauricio292@outlook.com>
2020-08-11 18:00:24 -0500

    subir del hijo al padre :tada:

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css'
 //screens
 import BlogNotes from "./screens/NotesBlog"
+import CustomTextField from "./components/CustomTextField"
 function App() {
   return (
     <div className="App">
