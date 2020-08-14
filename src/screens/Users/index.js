b477ad37792f7684e632abf97a21d9e07bfbba88
mauricio <castillo_smauricio292@outlook.com>
2020-08-13 19:18:58 -0500

    formulario de usuarios

--- src/screens/Users/index.js
+++ src/screens/Users/index.js
@@ -1,12 +1,77 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+
+// Custom components
+import UserForm from "./components/UserForm";
 
 class Users extends Component {
-  render() {
-    return (
-      <h1>Usuarios</h1>
-    )
+  constructor(props) {
+    super(props);
+    this.state = {
+      users: [],
+    };
+    this.handleForm = this.handleForm.bind(this);
+  }
 
+  componentDidMount() {
+    console.log("Componente montado correctamente");
+    fetch("https://reactsessions-ac545.firebaseio.com/.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then(({ users }) => {
+        let parsedUsers = [];
+        for (let key in users) {
+          let user = users[key];
+          user["key"] = key;
+          parsedUsers.push(user);
+        }
+        if (parsedUsers) {
+          this.setState({
+            users: parsedUsers,
+          });
+        }
+      });
+  }
+
+  handleForm(firstName, lastName, age) {
+    fetch("https://reactsessions-ac545.firebaseio.com/users.json", {
+      method: "POST",
+      mode: "cors",
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        age,
+      }),
+    })
+      .then((response) => response.json())
+      .then(({ name }) => {
+        const { users } = this.state;
+        const newUsers = [...users, { firstName, lastName, key: name }];
+        this.setState({
+          users: newUsers,
+        });
+      });
+  }
+
+  render() {
+    const { users } = this.state;
+    const UIUsers = users.map(({ firstName, lastName }, index) => (
+      <li key={index}>
+        {firstName} {lastName}
+      </li>
+    ));
+
+    return (
+      <div className="Container">
+        {UIUsers.length ? null : <p>No hay usuarios</p>}
+        <ul>{UIUsers}</ul>
+        <UserForm callback={this.handleForm} />
+      </div>
+    );
   }
 }
+export default Users;
 
-export default Users
+
+
+
